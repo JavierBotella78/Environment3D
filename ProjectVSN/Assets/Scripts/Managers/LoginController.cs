28939ac808028c3c35ce1d506b689b1675249ab3
Javier Botella Martinez <78485165+JavierBotella78@users.noreply.github.com>
2022-08-02 17:35:12 +0200

    Cambios en el login controller y pruebas unitarias añadidas

--- ProjectVSN/Assets/Scripts/Managers/LoginController.cs
+++ ProjectVSN/Assets/Scripts/Managers/LoginController.cs
@@ -26,6 +26,8 @@ public static class LoginController
     {
         string[] lines = GetLoginFileLines();
 
+        
+        // Iteramos sobre todo el documento y comprobamos que el usuario exista
         foreach (string line in lines)
         {
             string tmpName = line.Substring(0, line.IndexOf(separator));
@@ -52,6 +54,7 @@ public static class LoginController
         {
             string[] lines = GetLoginFileLines();
 
+            // Comprobamos que el documento no esté vacio ni que el usuario ya exista
             if (lines.Length != 0)
             {
                 foreach (string line in lines)
@@ -60,11 +63,10 @@ public static class LoginController
 
                     if (tmpName == user)
                         return false;
-                    else
-                        break;
                 }
             }
 
+            // Encriptamos la contraseña y almacenamos usuario y contraseña
             string encPasw = EncryptString(pasw);
 
             AddUser(user, encPasw);
@@ -73,23 +75,66 @@ public static class LoginController
         return true;
     }
 
+    // Elimina un usuario pero es necesaria la contraseña
+    public static bool EraseUser(string user, string pasw)
+    {
+        // Comprobamos que el usuario exista
+        if (!CheckUser(user, pasw))
+            return false;
+
+        string[] lines = GetLoginFileLines();
+        string[] newLines = new string[lines.Length - 1];
+        int i = 0;
+
+        // Iteramos y añadimos a un nuevo string a todos menos al usuario que queremos eliminar
+        foreach (string line in lines)
+        {
+            string tmpName = line.Substring(0, line.IndexOf(separator));
+
+            if (tmpName != user)
+            {
+                newLines[i] = line;
+                i++;
+            }
+        }
+
+        SetLoginFileLines(newLines);
+
+        return true;
+    }
+
+    // Encriptamos la cadena de caracteres con la funcion SHA256
     private static string EncryptString(string chain)
     {
-        string encChain = Encoding.UTF8.GetString(encoder.ComputeHash(Encoding.UTF8.GetBytes(chain)));
+        byte[] encBytes = encoder.ComputeHash(Encoding.UTF8.GetBytes(chain));
+        string encChain = Convert.ToBase64String(encBytes);
 
         return encChain;
     }
 
+    // Devuelve el contenido del login.txt
     private static string GetLoginFileContent()
     {
         return System.IO.File.ReadAllText(loginPath);
     }
 
+    private static void SetLoginFileContent(string content)
+    {
+        System.IO.File.WriteAllText(loginPath, content, Encoding.UTF8);
+    }
+
+    // Devuelve el contenido del login.txt en forma de lineas string[]
     private static string[] GetLoginFileLines()
     {
         return System.IO.File.ReadAllLines(loginPath);
     }
 
+    private static void SetLoginFileLines(string[] lines)
+    {
+        System.IO.File.WriteAllLines(loginPath, lines, Encoding.UTF8);
+    }
+
+    // Añade un nuevo usuario y su contraseña en el documento de texto
     private static void AddUser(string name, string encPass)
     {
         string fileContent = "";
@@ -97,11 +142,11 @@ public static class LoginController
 
         if (GetLoginFileLines().Length == 0)
             offset = "";
-
-        fileContent += offset;
+        
         fileContent += name;
         fileContent += separator;
         fileContent += encPass;
+        fileContent += offset;
 
         System.IO.File.AppendAllText(loginPath, fileContent);
     }
